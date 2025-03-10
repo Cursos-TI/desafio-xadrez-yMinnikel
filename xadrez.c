1d2a468262dadc0d04c1bcc99750e0a09d4e6b4f
minnikel <142114689+yMinnikel@users.noreply.github.com>
2025-03-10 16:44:12 +0000

    commit xadrez intermediario

--- xadrez.c
+++ xadrez.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
 
